c1d34e3c4f2a38445ac701c83e1a5089b8f2e1a8
Ral-Zhao <405952631@qq.com>
2017-09-21 16:52:53 +0800

    2017-9-21

---
+++ src/findPeakElement162/Test.java
@@ -0,0 +1,10 @@
+package findPeakElement162;
+
+/**
+ * Created by Zhao Zhe on 2017/9/21.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/maximumSwap670/Test.java
@@ -0,0 +1,12 @@
+package maximumSwap670;
+
+/**
+ * Created by Zhao Zhe on 2017/9/21.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+        System.out.println(s.maximumSwap(98368));
+    }
+}
