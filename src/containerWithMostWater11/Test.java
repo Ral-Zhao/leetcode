c0657f1c6964ddbfb0b84f888529001db6b7f96a
Ral-Zhao <405952631@qq.com>
2017-09-22 19:54:42 +0800

    2017-9-22

---
+++ src/containerWithMostWater11/Test.java
@@ -0,0 +1,13 @@
+package containerWithMostWater11;
+
+/**
+ * Created by Zhao Zhe on 2017/9/22.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        Solution s = new Solution();
+        int[] nums = new int[]{4,6,4,4,6,2,6,7,11,2};
+        System.out.println(s.maxArea(nums));
+    }
+}
