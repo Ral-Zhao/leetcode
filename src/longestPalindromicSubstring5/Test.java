64db3943c58da0325b0f65993504aa12334975aa
zhaozhezijian <zhaozhezijian@jd.com>
2019-04-24 13:41:23 +0800

    20190424

---
+++ src/longestPalindromicSubstring5/Test.java
@@ -0,0 +1,9 @@
+package longestPalindromicSubstring5;
+
+public class Test {
+    public static void main(String[] args) {
+        String temp = "fafgege";
+        Solution solution = new Solution();
+        System.out.println(solution.solutionDP(temp));
+    }
+}
---
+++ src/longestSubstringWithoutRepeatingCharacters3/Test.java
@@ -0,0 +1,10 @@
+package longestSubstringWithoutRepeatingCharacters3;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String str = "abcabcaa";
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring(str));
+    }
+}
